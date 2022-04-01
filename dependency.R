9ba87f1f665ba369dbbd3dfb99f9f39988429111
Shivani Srivastava <shivani141296@gmail.com>
2022-04-01 18:18:36 +0530

    Create dependency.R

---
+++ dependency.R
@@ -0,0 +1,14 @@
+if(!require("shiny")) {install.packages("shiny")}
+if(!require("tools")) {install.packages("tools")}
+if(!require("DT")) {install.packages("DT")}
+if(!require("data.table")) {install.packages("data.table")}
+if(!require("stringr")) {install.packages("stringr")}
+if(!require("udpipe")) {install.packages("udpipe")}
+if(!require("doc2vec")) {install.packages("doc2vec")}
+library(shiny)
+library(doc2vec)
+library(udpipe)
+library(stringr)
+library(data.table)
+library(DT)
+library(tools)
